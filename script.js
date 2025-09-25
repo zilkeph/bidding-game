9a5af2cde6b591e0f9d379ec7e540bdf2671fa85
zilkeph <zilkeph@gmail.com>
2025-09-25 16:22:23 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById('testButton').addEventListener('click', function() {
+    document.getElementById('message').textContent = 'The button works! Your hosting is set up correctly.';
+});
