a152acc6a77bad716038a315e6fb69c33eccf315
zilkeph <zilkeph@gmail.com>
2025-09-25 16:21:56 +0200

    Delete Bidding Game directory

--- Bidding Game/script.js
+++
@@ -1,3 +0,0 @@
-document.getElementById('testButton').addEventListener('click', function() {
-    document.getElementById('message').textContent = 'The button works! Your hosting is set up correctly.';
-});
